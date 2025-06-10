0ee655d3037cce66d0a2606a8dd8bed13a3b0d29
urgen-lama <24046545@saultcollege.ca>
2025-06-10 19:56:21 -0400

    Board of TicTacToe

--- src/main/java/lab3/game/Board.java
+++ src/main/java/lab3/game/Board.java
@@ -1,5 +1,39 @@
 package lab3.game;
 
 public class Board {
-    // TODO: encapsulate the representation of the tictactoe board and provide instance methods to access and update it
+    private char[][] board;
+
+    public Board() { //Using 2D array because it is easy to access and modify
+        board = new char[][] {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+    }
+
+    public void printBoard() {
+        System.out.println("\n---------------");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(" | ");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n---------------");
+        }
+    }
+
+    public boolean makeMove(int row, int col, char player) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = player;
+            return true;
+        }
+        return false;
+    }
+
+    public char getCell(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+            return board[row][col];
+        }
+        return ' ';
+    }
 }
